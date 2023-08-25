b0d6ea73543f315a80b399064792022315912922
Kanha <Kanhu.sahoo09@gmail.com>
2023-08-25 17:43:06 +0530

    Update app.js

--- client/src/app.js
+++ client/src/app.js
@@ -2,7 +2,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "./Pages/Home"
 import Login from "./Pages/Login"
 import Register from "./Pages/Register"
+import { useSelector } from "react-redux"
 function App() {
+  const { user } = useSelector((state) => ({ ...state.user }))
+  console.log(user)
   return (
     <div className="dark">
       <Router>
