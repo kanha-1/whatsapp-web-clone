cc73cb7c235fa3337a8799289e73c95c275e1a85
Kanha <Kanhu.sahoo09@gmail.com>
2023-08-25 17:42:26 +0530

    Create Validation.js

---
+++ client/src/utils/Validation.js
@@ -0,0 +1,9 @@
+import * as Yup from "yup"
+
+export const signupSchema = Yup.object({
+    name: Yup.string().required("Name is Required !").matches(/^[a-zA-Z_ ]*$/, "No Special Characters Allowed")
+        .min(3, "Please Enter More Than 3 characters"),
+    email: Yup.string().required("Email Is Reqyired").email("invalid email address"),
+    status: Yup.string().max(64, "Status must be less than 64 characters. !"),
+    password: Yup.string().required("Password Required")
+})
