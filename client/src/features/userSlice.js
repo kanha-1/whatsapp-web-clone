625766f93a9f1654a7915c927d9b8d42d0f3c238
Kanha <Kanhu.sahoo09@gmail.com>
2023-08-25 17:41:00 +0530

    Update userSlice.js

--- client/src/features/userSlice.js
+++ client/src/features/userSlice.js
@@ -1,4 +1,7 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios"
+// const AUTH_URL = `${process.env.BASE_URL}`
+const AUTH_URL = `http://localhost:8080/api`
 
 const initialState = {
     status: "",
@@ -12,7 +15,14 @@ const initialState = {
         status: ""
     }
 }
-
+export const registerUser = createAsyncThunk('auth/register', async (values, { rejectWithValue }) => {
+    try {
+        const { data } = await axios.post(`${AUTH_URL}/auth/register`, { ...values })
+        return data
+    } catch (error) {
+        return rejectWithValue(error.response.data.error.message)
+    }
+})
 export const userSlice = createSlice({
     name: "user",
     initialState,
@@ -29,6 +39,20 @@ export const userSlice = createSlice({
                 status: ""
             }
         }
+    },
+    extraReducers(builder) {
+        builder.addCase(registerUser.pending, (state, action) => {
+            state.status = "loading"
+        })
+            .addCase(registerUser.fulfilled, (state, action) => {
+                state.status = "success"
+                state.user = action.payload?.user
+                state.error = ""
+            })
+            .addCase(registerUser.rejected, (state, action) => {
+                state.status = "failed"
+                state.error = action.payload
+            })
     }
 })
 
