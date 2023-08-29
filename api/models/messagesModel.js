3c9d1f93e226e770bb0a5c072ce611bc1fc1635d
Kanhu charan sahoo <Kanhu.sahoo09@gmail.com>
2023-08-29 16:25:47 +0530

    Update messagesModel.js
    added schema for messages

--- api/models/messagesModel.js
+++ api/models/messagesModel.js
@@ -1,1 +1,23 @@
+import mongose from "mongoose"
+const { ObjectId } = mongose.Schema.Types
+const MessageSchema = mongose.Schema({
+    sender: {
+        type: ObjectId,
+        ref: "UserModel"
+    },
+    message: {
+        type: String,
+        trim: true
+    },
+    conversation: {
+        type: ObjectId,
+        ref: "ConversationModel"
+    },
+    files: []
+}, {
+    collection: "messages",
+    timestamps: true
+})
 
+const MessageModel = mongose.models.MessageModel || mongose.Schema("MessageModel", MessageSchema)
+export default MessageModel 
