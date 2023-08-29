18593fbfc173059a3b3db9472fffb021e3e491d4
Kanhu charan sahoo <Kanhu.sahoo09@gmail.com>
2023-08-29 16:19:31 +0530

    Update conversation Routes

--- api/routes/conversationRoutes.js
+++ api/routes/conversationRoutes.js
@@ -1,8 +1,10 @@
 import express from "express"
 import trimRequest from "trim-request"
 import authMiddleware from "../middlewares/authMiddleware.js"
+import { create_new_conversation, getConversation } from "../controllers/conversationController.js"
 const router = express.Router()
 
-router.route('/').post(trimRequest.all,authMiddleware)
+router.route('/').post(trimRequest.all, authMiddleware, create_new_conversation)
+router.route('/').get(trimRequest.all, authMiddleware, getConversation)
 
 export default router;
