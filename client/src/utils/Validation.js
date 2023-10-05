c825d062f7d06b5a969f5f537e8849a06d287a0d
Kanhu charan sahoo <Kanhu.sahoo09@gmail.com>
2023-10-05 12:35:50 +0530

    Updated utils

--- client/src/utils/Validation.js
+++ client/src/utils/Validation.js
@@ -7,3 +7,7 @@ export const signupSchema = Yup.object({
     status: Yup.string().max(64, "Status must be less than 64 characters. !"),
     password: Yup.string().required("Password Required")
 })
+export const signinSchema = Yup.object({
+    email: Yup.string().required("Email Is Reqyired").email("invalid email address"),
+    password: Yup.string().required("Password Required")
+})
---
+++ client/src/utils/bytesConverter.js
@@ -0,0 +1,10 @@
+export const bytesToKB = (bytes) => {
+    if (bytes < 1024) {
+        return bytes + ' B';
+    } else if (bytes < 1024 * 1024) {
+        return (bytes / 1024).toFixed(2) + ' KB';
+    } else {
+        return (bytes / (1024 * 1024)).toFixed(2) + ' MB';
+    }
+
+}
