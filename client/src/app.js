5ab716de11416b289e4d2b972cf1313b69df7c45
Kanhu charan sahoo <Kanhu.sahoo09@gmail.com>
2023-10-05 12:35:14 +0530

    Update app.js

--- client/src/app.js
+++ client/src/app.js
@@ -1,20 +1,28 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
+import { useSelector } from "react-redux"
+import { io } from "socket.io-client";
+import SocketContext from "./context/SocketContext";
 import Home from "./Pages/Home"
 import Login from "./Pages/Login"
 import Register from "./Pages/Register"
-import { useSelector } from "react-redux"
+// socket.io connection
+const socket = io("http://localhost:8080")
+
 function App() {
   const { user } = useSelector((state) => ({ ...state.user }))
-  const access_token = user?.access_token
+  const { files } = useSelector((state) => (state.chat))
+  const access_token = user?.token
   return (
     <div className="dark">
-      <Router>
-        <Routes>
-          <Route exact path="/" element={access_token ? <Home /> : <Navigate to="/login" />} />
-          <Route exact path="/register" element={!access_token ? <Register /> : <Navigate to="/" />} />
-          <Route exact path="/login" element={!access_token ? <Login /> : <Navigate to="/" />} />
-        </Routes>
-      </Router>
+      <SocketContext.Provider value={socket}>
+        <Router>
+          <Routes>
+            <Route exact path="/" element={access_token ? <Home socket={socket} /> : <Navigate to="/login" />} />
+            <Route exact path="/register" element={!access_token ? <Register /> : <Navigate to="/" />} />
+            <Route exact path="/login" element={!access_token ? <Login /> : <Navigate to="/" />} />
+          </Routes>
+        </Router>
+      </SocketContext.Provider>
     </div>
   );
 }
